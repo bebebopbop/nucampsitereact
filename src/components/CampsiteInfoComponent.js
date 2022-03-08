a506d36dfc2b55df75cd3946e91c54ee3006fcef
renitagidlund <renita.y.ip@gmail.com>
2022-03-07 21:03:17 -0800

    Functional Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite) {
+
+function RenderCampsite({campsite}) {
       return(
       <div className="col-md-5 m-1">
         <Card>
@@ -15,7 +15,7 @@ class CampsiteInfo extends Component {
         </Card>
         </div>)
     }
-    renderComments(comments) {
+    function RenderComments({comments}) {
       if(comments){
         return (
         <div className="col-md-5 m-1">
@@ -34,18 +34,19 @@ class CampsiteInfo extends Component {
       }
       return <div />
     }
-    render() {
-        if (this.props.campsite) {
-          return(
-            <div className="container">
-              <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
+
+  function CampsiteInfo(props) {
+      if (props.campsite) {
+          return (
+              <div className="container">
+                  <div className="row">
+                      <RenderCampsite campsite={props.campsite} />
+                      <RenderComments comments={props.campsite.comments} />
+                  </div>
               </div>
-            </div>
-                );
-        }
-        return <div />;
+          );
       }
-    }
-    export default CampsiteInfo;
+      return <div />;
+  }
+
+  export default CampsiteInfo;
