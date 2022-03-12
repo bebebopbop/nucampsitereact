4cae69d575f9b6fa311d19ea240eecfbf4346920
renitagidlund <renita.y.ip@gmail.com>
2022-03-12 11:36:18 -0800

    workshop updates to about

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -7,7 +7,9 @@ function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
 
@@ -71,6 +73,23 @@ function About(props) {
             </div>
         </div>
     );
+}
+
+function RenderPartner({partner}) {
+    if (partner) {
+        return (
+            <>   
+             <Media object src={partner.image} alt={partner.name} width="150"/>  
+             <Media body className="ml-5 mb-4">
+                <Media heading>
+                    {partner.name}
+                </Media>
+                {partner.description}
+            </Media>     
+            </>
+        )
+    }
+       return <div />
 }
 
 export default About;
